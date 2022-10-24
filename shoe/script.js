6825c2eb816212032be9afe832f6183753369b29
KhalidIrfani <107645929+KhalidIrfani@users.noreply.github.com>
2022-10-25 00:35:10 +0500

    cmt

---
+++ shoe/script.js
@@ -0,0 +1,16 @@
+var element;
+
+// element=document.URL
+// element=document.domain
+// element=document.baseURI
+element=document.getElementById("h1").innerText
+element=document.getElementById("sex").getAttributeNode("id")
+// element=document.getElementById("h1")
+// element=document.getElementsByClassName("Section1").innerText
+// // element=document.getElementsByClassName("card")[9];
+
+// element=document.getElementsByTagName("ul")
+
+
+
+console.log(element)
